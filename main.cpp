303639315561a38a3d0e12334e6a9089198a4968
okovtun <31523835+okovtun@users.noreply.github.com>
2019-05-18 15:51:42 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int Arr[], const int n);
+void Print(int Arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int Arr[n];
+
+}
+
+void FillRand(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		Arr[i] = rand() % 100;
+	}
+}
+
+void Print(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << Arr[i] << "\t";
+	}
+	cout << endl;
+}
